6b5159fd6f84e1df2840a48ce8a4c1d0e74c4235
CallumGame <callumutd@live.co.uk>
2023-11-28 16:19:25 +0300

    made a function that plays an audio on all the buttons but only one audio

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+// var drums = document.querySelectorAll(".drum");
+
+// for( var i = 0; i < drums.length; i++){
+//     drums[i].addEventListener("click", function (){
+//     alert("i got clicked")
+// });
+// }
+
 var drums = document.querySelectorAll(".drum");
 
+var audio = new Audio('sounds/tom-1.mp3');
+
 for( var i = 0; i < drums.length; i++){
+   
     drums[i].addEventListener("click", function (){
-    alert("i got clicked")
-});
+    audio.play();
+    });
+
 }
