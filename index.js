cdf8243cd24a8c6c3e9e7be2cfbf85a34e11dc7a
CallumGame <callumutd@live.co.uk>
2023-12-06 03:15:30 +0300

    deleted comments as switch statement works

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// var drums = document.querySelectorAll(".drum");
-
-// for( var i = 0; i < drums.length; i++){
-//     drums[i].addEventListener("click", function (){
-//     alert("i got clicked")
-// });
-// }
-
-// var drums = document.querySelectorAll(".drum");
-
-// var audio = new Audio('sounds/tom-1.mp3');
-
-// for( var i = 0; i < drums.length; i++){
-   
-//     drums[i].addEventListener("click", function (){
-//     audio.play();
-//     });
-
-// }
-
 var drums = document.querySelectorAll(".drum");
 
 for( var i = 0; i < drums.length; i++){
